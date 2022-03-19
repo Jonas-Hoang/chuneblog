60acab9d26e545f30e7ea71af1db942808b9509a
Jonas-Hoang <bangbang3951@gmail.com>
2022-03-19 17:10:31 +0700

    new

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+
+import {SubHeading} from '../../components';
+import {images} from '../../constants';
+import {BsInstagram, BsArrowLeftShort, } from 'react-icons/bs';
 import './Gallery.css';
 
 const Gallery = () => (
-  <div>
-    Gallery
+  <div className='app__gallery flex__center'>
+    <div className='app__gallery-content'>
+      <SubHeading tittle='Instagram'/>
+      <h1 className='headtext__cormorant'>Photo Gallery</h1>
+      <p className='p__cormorant flex__center' style={{ color:'#aaa',marginTop:'2rem' }}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.
+      </p>
+      <div className='app__button' style={{ marginTop:'2rem' }}>
+      <button className='custom__button' type='button'>View more</button>
+      </div>
+      
+    </div>
+    
   </div>
 );
 
